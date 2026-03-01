2183719dd6cb2eaf6986e9d16426f4406b2bd606
Lenovo <vaibhavkanade03@gmail.com>
2026-03-01 13:50:31 +0530

    Project cleanup

--- src/main.jsx
+++ src/main.jsx
@@ -1,14 +1,2 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import axios from 'axios'
-import App from './App.jsx'
-import './index.css'
-
-// Configure global API base URL from environment
-axios.defaults.baseURL = import.meta.env.API_BASE_URL || 'http://localhost:5000'
-
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+// Legacy entry point stubbed for build.
+export default {};
