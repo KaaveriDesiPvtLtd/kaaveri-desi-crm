e321874e71a2605186d7e0212281609eb7133ffe
Lenovo <vaibhavkanade03@gmail.com>
2026-03-01 15:25:59 +0530

    vercal config

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,15 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     reactStrictMode: true,
+    async rewrites() {
+        const backendUrl = process.env.NEXT_PUBLIC_API_URL || 'https://api-kaaveri-desi.vercel.app';
+        return [
+          {
+            source: '/api/crm/:path*',
+            destination: `${backendUrl}/api/crm/:path*`,
+          },
+        ];
+    },
 }
 
 export default nextConfig
